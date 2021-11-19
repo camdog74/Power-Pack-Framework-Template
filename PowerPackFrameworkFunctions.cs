92c9b713497082317d7d58136d758c8a3bef3063
camdog74 <59524660+camdog74@users.noreply.github.com>
2021-11-19 23:39:12 +1100

    New PPF Checking system (thanks aspa102!)

--- PowerPackFrameworkFunctions.cs
+++ PowerPackFrameworkFunctions.cs
@@ -7,12 +7,14 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 using System.Reflection;
+using UnityEngine.SceneManagement;
 
 namespace PPF
 {
-    
     public class PowerPackFrameworkFunctions
     {
+        public static bool DoingThings = false;
+
         public static void CreateCape(PersonBehaviour person, Sprite CapeTexture,float CapeThickness = 0.15f, Sprite CapeBaseSprite = null)
         {
             List<object> CapeInfo = new List<object>() { person, CapeTexture,CapeThickness, CapeBaseSprite };
@@ -25,8 +27,6 @@ namespace PPF
             }
 
         }
-
-
 
         public static void AddSkin(PersonBehaviour person,Texture2D texture, string SkinName, string Description)
         {
@@ -56,10 +56,33 @@ namespace PPF
         {
             if (!GameObject.Find("PowerPackFrameworkManager"))
             {
-                GameObject UrlObject = new GameObject();
-                UrlObject.AddComponent<URLOpenBehaviour>();
-                DialogBoxManager.Dialog("POWER PACK FRAMEWORK NOT INSTALLED/ENABLED\nYour mods won't work properly without it", new DialogButton("Close", true), new DialogButton("Workshop Page", true, new UnityAction[1] { (UnityAction)(() => { UrlObject.GetComponent<URLOpenBehaviour>().OpenURL("https://steamcommunity.com/workshop/filedetails/?id=2506978276"); }) }));
+                if (!GameObject.Find("AThingForThingsPowerPackThing"))
+                {
+                    GameObject UrlObject = new GameObject("AThingForThingsPowerPackThing");
+                    UrlObject.AddComponent<URLOpenBehaviour>();
+                    foreach (var mod in ModLoader.LoadedMods)
+                    {
+                        if (mod.CreatorUGCIdentity == "2506978276")
+                        {
+                            DialogBoxManager.Dialog("POWER PACK FRAMEWORK NOT ENABLED\nYour mods won't work properly without it", new DialogButton("Close", true), new DialogButton("Enable and Reload", true, new UnityAction[1] { (UnityAction)(() =>
+                            {
+                                if (!DoingThings)
+                                {
+                                    DoingThings = true;
+                                    ModLoader.SetModActive(mod);
+                                    SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex).completed += Done;
+                                }
+                            })}));
+                            return;
+                        }
+                    }
+                    DialogBoxManager.Dialog("POWER PACK FRAMEWORK NOT INSTALLED\nYour mods won't work properly without it", new DialogButton("Close", true), new DialogButton("Workshop Page", true, new UnityAction[1] { (UnityAction)(() => { UrlObject.GetComponent<URLOpenBehaviour>().OpenURL("https://steamcommunity.com/workshop/filedetails/?id=2506978276"); }) }));
+                }
             }
+        }
+        private static void Done(AsyncOperation op)
+        {
+            DoingThings = false;
         }
     }
 }
